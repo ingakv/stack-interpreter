98ac342561e6fa68dc06af9fd5635b4606ebf958
ingakv <ingakv@stud.ntnu.no>
2023-04-07 21:29:25 +0200

    Feature: Implemented the stack operations

--- src/mylib.rs
+++ src/mylib.rs
@@ -4,7 +4,7 @@ use std::io::{Write};
 
 pub fn slay() {
 
-    let mut str: Vec<String> = Vec::new();
+    let mut stack: Vec<String> = Vec::new();
 
     loop {
         print!("bprog> ");
@@ -18,18 +18,51 @@ pub fn slay() {
 
         let newEl: Vec<&str> = input.split_whitespace().collect();
 
+
         println!("Stack: ");
         for i in newEl {
-            str.push(i.parse().unwrap());
+            stack = checkForOperator(i, stack.clone());
         }
 
 
-
-        for i in str.iter().rev() {
+        for i in stack.iter().rev() {
             println!("{}",i);
         }
-
 
     }
 
 }
+
+
+fn checkForOperator(elem : &str, mut stack: Vec<String>) -> Vec<String> {
+
+    // dup duplicates the top element
+    if elem == "dup" {
+        if let Some(str_ref) = stack.last() {
+            let str_val: String = str_ref.to_owned();
+            stack.push(str_val);
+
+        } else {}
+    }
+
+    // swap swaps the top two elements
+    else if elem == "swap" {
+        let len = stack.len();
+        if len > 1 {stack.swap(len-1, len);} else {}
+    }
+
+    // pop removes the top element
+    else if elem == "pop" {
+        stack.pop();
+    }
+
+    // If a stack operation was not typed in, push the value to the stack
+    else {
+        stack.push(elem.to_string());
+    }
+
+    // Return the stack
+    stack
+}
+
+
