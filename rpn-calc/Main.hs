01ec50112a887aeab7220cade931a3f80bea7e5d
ingakv <ingakv@stud.ntnu.no>
2023-04-28 21:34:45 +0200

    Docs: Added README

--- rpn-calc/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Lib
-
--- | Main REPL for the program. We treat each line as a separate "program".
--- We do not keep "state" across the different lines.
-main :: IO ()
-main = interact $ unlines . map processLine . lines
